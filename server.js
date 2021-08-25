8e3a307d563aa691be2b518e103e1d59fb58cda2
Ghefest <ghefestg@gmail.com>
2021-08-26 01:05:49 +0300

    dev

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port = 3000;
 console.log(DetectService.redirectFlow);
 
 app.use(DetectService.redirectFlow);
-app.use('/black', express.static(path.join(__dirname, 'black')));
 app.use('/white', express.static(path.join(__dirname, 'white')));
+app.use('/black', express.static(path.join(__dirname, 'black')));
 
 app.listen(3000, () => console.log('Server running on port ' + port));

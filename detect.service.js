2c7a7f3f9b5f1ca7f77ff18fb3e9ca730dc6cd0a
Ghefest <ghefestg@gmail.com>
2021-08-26 02:11:06 +0300

    TODO: upd

--- detect.service.js
+++ detect.service.js
@@ -3,17 +3,16 @@ const geoip = require('geoip-lite');
 module.exports = class DetectService {
   constructor() {
     this.ip = '';
+    this.referer = '';
   }
+
+  checkReferer() {}
 
   isWhiteListCountry() {
     console.log(this.getCountry(), 'Remote country');
     console.log(process.env.WHITE_LIST_COUNTRIES, 'White list');
 
     return this.getCountry() === process.env.WHITE_LIST_COUNTRIES;
-  }
-
-  isWhite() {
-    return !this.isWhiteListCountry();
   }
 
   getCountry() {
@@ -34,8 +33,15 @@ module.exports = class DetectService {
     return lookedUpIP.country;
   }
 
+  isWhite() {
+    return !this.isWhiteListCountry();
+  }
+
   redirectFlow(req, res, next) {
     this.ip = req.connection.remoteAddress;
+    this.referer = req.headers.referer;
+
+    console.log('Referer:', this.referer);
 
     const isWhite = this.isWhite();
     console.log(isWhite);

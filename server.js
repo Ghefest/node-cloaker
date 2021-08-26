8561a186b4b88d9b5a733aa747a799ea24c8e22a
Ghefest <ghefestg@gmail.com>
2021-08-26 04:40:14 +0300

    feat: move port to .env variables

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 'use strict';
 
 require('dotenv').config();
+const PORT = process.env.PORT;
 
 const fs = require('fs');
 const path = require('path');
 
 const DetectService = require('./detect.service.js');
+const CountersService = require('./counters.service.js');
 
 const express = require('express');
 
 const app = express();
 const useragent = require('express-useragent');
-const port = 3000;
 
 app.use(useragent.express());
 app.use((req, res, next) => new DetectService().redirectFlow(req, res, next));
+app.use((req, res, next) => new CountersService().countFlow(req, res, next));
 app.use('/white', express.static(path.join(__dirname, 'white')));
 app.use('/black', express.static(path.join(__dirname, 'black')));
 
-app.listen(3000, () => console.log('Server running on port ' + port));
+app.listen(+PORT, () => console.log('Server running on port ' + PORT));
